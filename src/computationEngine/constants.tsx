1a5fcfc0d759cc9f712011ada8dfd84a0e99e9f3
NH0 <thibaultf@sipios.com>
2019-09-18 18:34:31 +0200

    Feat(types) create types for computation engine

---
+++ src/computationEngine/constants.tsx
@@ -0,0 +1,7 @@
+export const fundsArray = [{
+    isin: 12,
+    name: "Ocean",
+    history: [0.5, 0.2, -0.6, -0.2],
+    externalities: [],
+    description: ""
+}];
---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export type externalityName = 'animals'|'forest';
+
+export interface externality {
+    name: externalityName,
+    score: number
+};
+
+export type externalities = Array<externality>;
+
+export type userChoice = {
+    [name in externalityName]: {
+        value: number;
+    };
+};;
+
+export interface fund {
+    isin: number,
+    name: string,
+    history: Array<number>,
+    externalities: externalities,
+    description: string
+}
+
+export type funds = Array<fund>;
+
+export type expectation = Array<number>;
+
+export type covarianceMatrix = Array<Array<number>>;
+
+export type portfolio = Array<number>;
+
+export interface portfolioContentObject {
+    fund: fund,
+    weight: number
+}
+
+export interface response {
+    total: {
+      efficiency: number,
+      volatility: number,
+    },
+    graph: {
+      years: Array<number>,
+      meanEvolution: Array<number>,
+      optimisticEvolution: Array<number>,
+      pessimisticEvolution: Array<number>,
+    },
+    portfolioContent: Array<portfolioContentObject>,
+  };
