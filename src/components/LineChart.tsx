5d02b482ef55649c9b67cfcbcdb3b8308db98dda
Clara Lecroisey <clalecroisey@gmail.com>
2019-09-19 10:27:11 +0200

    Add colors for each dataset

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,4 +1,4 @@
-import { ChartData } from 'chart.js';
+import { ChartData, ChartDataSets } from 'chart.js';
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
@@ -8,44 +8,66 @@ interface Props {
   graph: ResultData['graph']
 };
 
+const datasetSample: ChartDataSets = {
+  fill: false,
+  lineTension: 0.1,
+  borderCapStyle: 'butt',
+  borderDash: [],
+  borderDashOffset: 0.0,
+  borderJoinStyle: 'miter',
+  pointBackgroundColor: '#fff',
+  pointBorderWidth: 1,
+  pointHoverRadius: 5,
+  data: [0, 0, 0, 0, 0, 0, 0]
+}
+
 export class LineChart extends React.Component<Props, ChartData> {
   constructor(props: Props) {
     super(props);
 
     this.state = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Evolution moyenne',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
+      datasets: []
     };
   }
 
   componentDidMount() {
+    const newDatasets = [
+      {
+        ...datasetSample,
+        label: 'Evolution moyenne',
+        data: this.props.graph.meanEvolution,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+      },
+      {
+        ...datasetSample,
+        label: 'Evolution optimiste',
+        data: this.props.graph.optimisticEvolution,
+        backgroundColor: 'rgba(233, 62, 145, 0.4)',
+        borderColor: 'rgba(233, 62, 145, 1)',
+        pointBorderColor: 'rgba(233, 62, 145, 1)',
+        pointHoverBackgroundColor: 'rgba(233, 62, 145, 1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+      },
+      {
+        ...datasetSample,
+        label: 'Evolution pessimiste',
+        data: this.props.graph.pessimisticEvolution,
+        backgroundColor: 'rgba(0,204,102,0.4)',
+        borderColor: 'rgba(0,204,102,1)',
+        pointBorderColor: 'rgba(0,204,102,1)',
+        pointHoverBackgroundColor: 'rgba(0,204,102,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+      }
+    ];
+
     this.setState({
       labels: this.props.graph.years.map(year => year + ''),
-      // datasets: [
-      //   {
-      //     ...this.state.datasets[0],
-      //     data: this.props.graph.meanEvolution,
-      //   }
-      // ]
+      datasets: newDatasets,
     })
   }
 
