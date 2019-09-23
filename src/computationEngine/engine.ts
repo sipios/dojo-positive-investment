6b3a3643b6a881a309ff85514fa7d9fe460490d9
NH0 <thibaultf@protonmail.com>
2019-09-23 11:45:52 +0200

    fix(engine) correct unsupported problem error
    Two funds had exactly the same data, the optimization algorithm didn't know how to choose between the two

--- src/computationEngine/engine.ts
+++ src/computationEngine/engine.ts
@@ -121,14 +121,10 @@ const computePortfolioAllocation = (
   const expectationMultiplierArray = computeRateReturnExpectationMultiplierArray(userChoice);
   const adaptedExpectationArray = computeAdaptedExpectationArray(expectationArray, expectationMultiplierArray);
 
-  const portfolioAllocation = PortfolioAllocation.meanVarianceOptimizationWeights(
-    adaptedExpectationArray.map(x => x + 0.000001 * Math.random()),
-    covariance,
-    {
-      optimizationMethod: 'maximumTargetVolatility',
-      constraints: { maxVolatility: maxVolatility },
-    },
-  );
+  const portfolioAllocation = PortfolioAllocation.meanVarianceOptimizationWeights(adaptedExpectationArray, covariance, {
+    optimizationMethod: 'maximumTargetVolatility',
+    constraints: { maxVolatility: maxVolatility },
+  });
 
   return portfolioAllocation;
 };
